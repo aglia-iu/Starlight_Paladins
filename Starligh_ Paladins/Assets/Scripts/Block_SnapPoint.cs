05e9c0bb44947e27c4e1f64da50799dde8c6e0ca
Anjali <agali1@iastate.edu>
2025-02-25 23:33:51 -0600

    Modified coding functionality for specific use case.

--- Starligh_ Paladins/Assets/Scripts/Block_SnapPoint.cs
+++ Starligh_ Paladins/Assets/Scripts/Block_SnapPoint.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Block_SnapPoint : MonoBehaviour
@@ -11,6 +12,17 @@ public class Block_SnapPoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        CollectBlocksofType();
+        //foreach (Block_Behavior_2D block in draggableObjs)
+        //{
+        //    block.dragEndedCallback = OnDragEnded;
+        //}
+    }
+
+    void Update()
+    {
+        CollectBlocksofType();
         foreach (Block_Behavior_2D block in draggableObjs)
         {
             block.dragEndedCallback = OnDragEnded;
@@ -40,6 +52,27 @@ public class Block_SnapPoint : MonoBehaviour
         if (closestSnapPoint != null && closestDistance <= snapRange)
         {
             block.transform.localPosition = closestSnapPoint.localPosition;
+        }
+    }
+
+    private void CollectBlocksofType()
+    {
+        GameObject[] snapObjects = GameObject.FindGameObjectsWithTag("codeSnap");
+        foreach (GameObject snapObject in snapObjects) 
+        {
+            if (!snapPoints.Contains(snapObject.transform))
+            {
+                snapPoints.Add(snapObject.transform);
+            }
+           
+        }
+        GameObject[] blockObjects = GameObject.FindGameObjectsWithTag("codeBlocks");
+        foreach (GameObject blockObject in blockObjects)
+        {
+            if (!draggableObjs.Contains(blockObject.GetComponent<Block_Behavior_2D>()))
+            {
+                draggableObjs.Add(blockObject.gameObject.GetComponent<Block_Behavior_2D>());
+            }
         }
     }
 }
