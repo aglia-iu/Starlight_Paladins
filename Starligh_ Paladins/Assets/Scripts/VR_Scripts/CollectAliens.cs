f55a2c8807059a633d1fd16dc257cb967915f799
Anjali <agali1@iastate.edu>
2025-05-06 00:55:27 -0500

    Added Axolotls, modified code panel, and added code to pickup and store axolotls in jar. Test Tomorrow.

---
+++ Starligh_ Paladins/Assets/Scripts/VR_Scripts/CollectAliens.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectAliens : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("I collided!");
+        if (collision.gameObject.tag == "alien")
+        {
+            Debug.Log("Caught an aliens!");
+            collision.gameObject.transform.parent = this.transform;
+            collision.gameObject.transform.position = new Vector3(0, 0, 0);
+        }
+    }
+}
